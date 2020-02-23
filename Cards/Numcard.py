d78455c9659cb2804a32bce60b00ae8aa6f33c46
Ceryus84 <61030434+Ceryus84@users.noreply.github.com>
2020-02-23 01:24:38 -0700

    Update Numcard.py
    Commenting, also final commit for the night.
    
    Need to work on inheritance for Numcard and Facecard.
    
    Too sleepy right now, will work more on Monday or Wednesday as time permits.

--- Cards/Numcard.py
+++ Cards/Numcard.py
@@ -5,6 +5,7 @@ class Numcard(Card):
     # subclass of Card, defines number cards
 
     def __init__(self, color, suit, number):
+        # a number card should have a color, a suit, and a number
         Card.__init__(self, color, suit)
         self.num = number
 
