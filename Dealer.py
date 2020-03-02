1999ab24e8747b471f836ca0e7ebe8b71ad17017
Ceryus84 <61030434+Ceryus84@users.noreply.github.com>
2020-03-02 00:52:31 -0700

    Making things easier and using new stuff.

---
+++ Dealer.py
@@ -0,0 +1,2 @@
+# dealer class will be defined here, will shuffle deck,
+# deal cards, and collect/doll out money.
---
+++ Table.py
@@ -0,0 +1,2 @@
+# table class will be defined here, table will hold dealer
+# player, and the deck
